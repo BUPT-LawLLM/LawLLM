80f6ed972dd24f7ca22db981fe484497c05f82f8
rickeyhhh <101970336+rickeyhhh@users.noreply.github.com>
2023-11-22 20:38:34 +0800

    Add files via upload

---
+++ keywords/keywords.py
@@ -0,0 +1,21 @@
+from transformers import AutoTokenizer, AutoModel
+from IPython.display import display, clear_output
+
+def keywords_extract(prompt):
+    
+    model_path = "/root/autodl-tmp/chatglm/ChatGLM2-6B/LoRA/keywords_extract"
+    tokenizer = AutoTokenizer.from_pretrained(model_path, trust_remote_code=True)
+    model = AutoModel.from_pretrained(model_path, trust_remote_code=True).quantize(4).half().cuda()
+    model = model.eval()
+    
+    #response = model.stream_chat(tokenizer, prompt, history=[])
+
+    for response, history in model.stream_chat(tokenizer, prompt, history=[]):
+        clear_output(wait=True)
+    
+    return response
+
+if __name__ == "__main__":
+    prompt = "有人未经我同意用我的照片在网上传播违法言论，我该做什么，根据上述内容，给出相关法律关键词"
+    output_text =  keywords_extract(prompt)
+    print(output_text)
